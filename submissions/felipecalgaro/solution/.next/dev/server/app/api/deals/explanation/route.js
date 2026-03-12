7c17649e6fa030c8e2ec56e115f806e69aab17b4
Felipe <pipecalgaro@gmail.com>
2026-03-12 14:19:01 +0100

    Remove Next.js build artifacts from submission

--- submissions/felipecalgaro/solution/.next/dev/server/app/api/deals/explanation/route.js
+++
@@ -1,7 +0,0 @@
-var R=require("../../../../chunks/[turbopack]_runtime.js")("server/app/api/deals/explanation/route.js")
-R.c("server/chunks/node_modules_next_e9661abd._.js")
-R.c("server/chunks/node_modules_openai_25629800._.js")
-R.c("server/chunks/[root-of-the-server]__302239ee._.js")
-R.c("server/chunks/_next-internal_server_app_api_deals_explanation_route_actions_75430af9.js")
-R.m("[project]/node_modules/next/dist/esm/build/templates/app-route.js { INNER_APP_ROUTE => \"[project]/app/api/deals/explanation/route.ts [app-route] (ecmascript)\" } [app-route] (ecmascript)")
-module.exports=R.m("[project]/node_modules/next/dist/esm/build/templates/app-route.js { INNER_APP_ROUTE => \"[project]/app/api/deals/explanation/route.ts [app-route] (ecmascript)\" } [app-route] (ecmascript)").exports
--- submissions/felipecalgaro/solution/.next/server/chunks/ssr/_next-internal_server_app_pipeline_page_actions_37453c62.js
+++
@@ -1,3 +0,0 @@
-module.exports=[95926,(a,b,c)=>{}];
-
-//# sourceMappingURL=_next-internal_server_app_pipeline_page_actions_37453c62.js.map
